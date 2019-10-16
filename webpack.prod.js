70f42b3d7a9baf668c3dc1c48c6cc8ca2d5d1006
Jennifer Chen <shinyi.chen@gmail.com>
2019-10-16 10:20:46 -0700

    feat: minify style

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,9 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,12 +12,20 @@ module.exports = {
         libraryTarget: 'var',
         library: 'Client'
     },
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    },
     module: {
         rules: [
             {
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+
+            {
+                test: /\.scss$/,
+                use: [{loader: MiniCssExtractPlugin.loader}, 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -22,6 +33,9 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
         })
     ]
 }
